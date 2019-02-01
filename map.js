790f2395ad2cd2ce204db5a0bfcef4ee1432b631
Ptolemytoo <42324696+Ptolemytoo@users.noreply.github.com>
2019-02-01 20:28:07 +0200

    Add files via upload

--- map.js
+++ map.js
@@ -1,3 +1,12 @@
 var map = new ol.Map({
         target: 'map',
-});
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat(20.019056, -33.600791]), // Coordinates of New York
+          zoom: 7 //Initial Zoom Level
+        })
+      });

e3d0536d3e71643be101587ea322aee09ad7efed
Ptolemytoo <42324696+Ptolemytoo@users.noreply.github.com>
2019-02-01 20:32:39 +0200

    Update map.js

--- map.js
+++ map.js
@@ -6,7 +6,7 @@ var map = new ol.Map({
           })
         ],
         view: new ol.View({
-          center: ol.proj.fromLonLat(20.019056, -33.600791]), // Coordinates of New York
+          center: ol.proj.fromLonLat([20.019056, -33.600791]), // Coordinates of New York
           zoom: 7 //Initial Zoom Level
         })
       });
